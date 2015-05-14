13f528e9dccbc84aeb4af702470334f70c23951f
Raphael Schweikert <any@sabberworm.com>
2015-05-14 21:41:07 +0200

    Add slack error handler
    Configure as follows:
    
    slack:
      test_integration:
        # This is the URL for your incoming webhook
        endpoint: https://hooks.slack.com/services/X/Y/z
        enabled: false
        # If not set, these options will fall back to your defaults
        icon_url: null
        icon_emoji: null
        channel: '#some-channel'
        # Valid: good, warning, danger, #rrggbb
        color: warning

---
+++ modules/filter/slack/SlackFilterModule.php
@@ -0,0 +1,75 @@
+<?php
+class SlackFilterModule extends FilterModule {
+	public function onAnyError($aContainer, $bNeverPrint = false, $bNeverNotifyDeveloper = false) {
+		if($bNeverNotifyDeveloper) {
+			return;
+		}
+		$aSlackConfigs = Settings::getSetting('slack', null, array());
+		$aError = &$aContainer[0];
+		foreach($aSlackConfigs as $sName => $aConfig) {
+			if(@$aConfig['enabled'] === false) {
+				continue;
+			}
+			$this->postToSlack($aError, $aConfig);
+		}
+	}
+	
+	private function postToSlack($aError, $aSlackConfig) {
+		$sURL = $aSlackConfig['endpoint'];
+		$sIconURL = @$aSlackConfig['icon_url'];
+		$sIconEmoji = @$aSlackConfig['icon_emoji'];
+		$sChannel = @$aSlackConfig['channel'];
+		$sColor = @$aSlackConfig['color'];
+
+		$aParams = array();
+		$aParams['text'] = $aError['message'];
+		if($sIconURL) {
+			$aParams['icon_url'] = $sIconURL;
+		}
+		if($sIconEmoji) {
+			$aParams['icon_emoji'] = $sIconEmoji;
+		}
+		if($sChannel) {
+			$aParams['channel'] = $sChannel;
+		}
+
+		$aErrorAttachment = array();
+		if($sColor) {
+			$aErrorAttachment['color'] = $sColor;
+		}
+		$aErrorAttachment['fields'] = array(
+			array(
+				'title' => 'Host',
+				'value' => $aError['host'],
+				'short' => false
+			),
+			array(
+				'title' => 'Path',
+				'value' => $aError['path'],
+				'short' => false
+			),
+			array(
+				'title' => 'Code',
+				'value' => $aError['code'],
+				'short' => true
+			),
+			array(
+				'title' => 'File',
+				'value' => $aError['filename'].' ('.$aError['line'].')',
+				'short' => false
+			),
+		);
+		$aErrorAttachment['text'] = implode("\n", $aError['trace']);
+
+		$aParams['attachments'] = array($aErrorAttachment);
+
+		$sParams = json_encode($aParams);
+
+		$rCurl = curl_init($sURL);
+		curl_setopt($rCurl, CURLOPT_POST, 1);
+		curl_setopt($rCurl, CURLOPT_POSTFIELDS, $sParams);
+		curl_setopt($rCurl, CURLOPT_HTTPHEADER, array('Content-Type' => 'application/json;charset=utf-8'));
+		curl_exec($rCurl);
+		curl_close($rCurl);
+	}
+}
